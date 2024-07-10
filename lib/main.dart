0eefde1ee31086603548b9568c24f178abaa07d0
Ridho Aulia Rahman <220605110044@student.uin-malang.ac.id>
2024-07-10 21:11:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration:const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+          child: const StartScreen()),
+      ),
+    ),
+  );
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // jika kita ingin membuat widget di dalam satu layar maka kita
+      // membuat dengan column
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
